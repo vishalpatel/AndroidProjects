7253179f403795574f4ef1ab1ba9ecb22ed1afe3
Vishal Patel <vpatel.apple@gmail.com>
2014-08-20 00:21:55 -0700

    Added functionality for saving list to text file and loading upon startup.

--- ShouldDo/src/com/vik/codepath/apps/todo/TodoActivity.java
+++ ShouldDo/src/com/vik/codepath/apps/todo/TodoActivity.java
@@ -1,6 +1,10 @@
 package com.vik.codepath.apps.todo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -23,7 +27,7 @@ public class TodoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
         lvItems = (ListView)findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
+        readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         items.add("First item");
@@ -34,6 +38,7 @@ public class TodoActivity extends Activity {
     public void addTodoItem(View v) {
     	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
     	itemsAdapter.add(etNewItem.getText().toString());
+    	saveItems();
     	etNewItem.setText("");
     }
     
@@ -45,9 +50,30 @@ public class TodoActivity extends Activity {
 					int position, long id) {
 				items.remove(position);
 				itemsAdapter.notifyDataSetChanged();
+				saveItems();
 				return false;
 			}
 		});
     }
     
+    private void readItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todolist.txt");
+    	try {
+			items = new ArrayList<String>(FileUtils.readLines(todoFile));
+		} catch (IOException e) {
+			items = new ArrayList<String>();
+			e.printStackTrace();
+		}
+    }
+    
+    private void saveItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todolist.txt");
+    	try {
+			FileUtils.writeLines(todoFile, items);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
 }
