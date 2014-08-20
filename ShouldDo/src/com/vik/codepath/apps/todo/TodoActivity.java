7c6e7ffb1d062ef2947530686255ac28821fba6e
Vishal Patel <vpatel.apple@gmail.com>
2014-08-20 02:49:09 -0700

    Adding support for sqlite database based saving of todo list.

--- ShouldDo/src/com/vik/codepath/apps/todo/TodoActivity.java
+++ ShouldDo/src/com/vik/codepath/apps/todo/TodoActivity.java
@@ -25,11 +25,15 @@ public class TodoActivity extends Activity {
 	ArrayList<String> items;
 	ArrayAdapter<String> itemsAdapter;
 	ListView lvItems;
+	DataAccessHelper itemsDAO;
+	ArrayList<TodoItem> TodoItemsList;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        itemsDAO = new DataAccessHelper(this);
+
         lvItems = (ListView)findViewById(R.id.lvItems);
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
@@ -40,7 +44,7 @@ public class TodoActivity extends Activity {
     public void addTodoItem(View v) {
     	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
     	itemsAdapter.add(etNewItem.getText().toString());
-    	saveItems();
+    	saveNewItem(new TodoItem(etNewItem.getText().toString()));
     	etNewItem.setText("");
     }
     
@@ -65,9 +69,9 @@ public class TodoActivity extends Activity {
 			public boolean onItemLongClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				String item_description = items.get(position);
+				removeItem(TodoItemsList.get(position));
 				items.remove(position);
 				itemsAdapter.notifyDataSetChanged();
-				saveItems();
 				Toast.makeText(getApplication(), "Removed " + item_description, Toast.LENGTH_SHORT).show();
 				return false;
 			}
@@ -81,28 +85,32 @@ public class TodoActivity extends Activity {
     		int position = data.getExtras().getInt("item_position");
     		items.set(position, item_desc);
     		itemsAdapter.notifyDataSetChanged();
-    		saveItems();
+    		updateItem(TodoItemsList.get(position), item_desc, false);
     	}
     }
     
     private void readItems() {
-    	File filesDir = getFilesDir();
-    	File todoFile = new File(filesDir, "todolist.txt");
-    	try {
-			items = new ArrayList<String>(FileUtils.readLines(todoFile));
-		} catch (IOException e) {
-			items = new ArrayList<String>();
-			e.printStackTrace();
+    	items = new ArrayList<String>();
+    	TodoItemsList = (ArrayList<TodoItem>) itemsDAO.getAllTodoItems();
+    	for (TodoItem t : TodoItemsList) {
+    		items.add(t.getDescription());
 		}
     }
     
-    private void saveItems() {
-    	File filesDir = getFilesDir();
-    	File todoFile = new File(filesDir, "todolist.txt");
-    	try {
-			FileUtils.writeLines(todoFile, items);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    private void saveNewItem(TodoItem t) {
+    	itemsDAO.addTodoItem(t);
+    	TodoItemsList.add(t);
     }
+    
+    private void removeItem(TodoItem t) {
+    	itemsDAO.removeTodoItem(t);
+    	TodoItemsList.remove(t);
+    }
+    
+    private void updateItem(TodoItem t, String desc, Boolean completed) {
+    	t.setDescription(desc);
+    	t.markCompleted(completed);
+    	itemsDAO.updateTodoItem(t);
+    }
+    
 }
