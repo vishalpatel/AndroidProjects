5cd30538fa7e199f0b78a9b37865ea5b1422f702
Vishal Patel <vpatel.apple@gmail.com>
2014-08-20 00:11:02 -0700

    Basic Todo app
    This app is made by following instructions.
    It does create and remove todoitems from list.
    Current version does not save items to file.

---
+++ ShouldDo/src/com/vik/codepath/apps/todo/TodoActivity.java
@@ -0,0 +1,53 @@
+package com.vik.codepath.apps.todo;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class TodoActivity extends Activity {
+	ArrayList<String> items;
+	ArrayAdapter<String> itemsAdapter;
+	ListView lvItems;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First item");
+        items.add("Second item");
+        setupListViewListener();
+    }
+
+    public void addTodoItem(View v) {
+    	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+    	itemsAdapter.add(etNewItem.getText().toString());
+    	etNewItem.setText("");
+    }
+    
+    private void setupListViewListener() {
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				items.remove(position);
+				itemsAdapter.notifyDataSetChanged();
+				return false;
+			}
+		});
+    }
+    
+}
